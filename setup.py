742d2a66edae05ef7f8b87cdad0ba0a59b6666b6
ucef08 <ucef08@gmail.com>
2023-07-07 11:37:58 +0200

    réparation des fichiers 9

--- setup.py
+++ setup.py
@@ -7,8 +7,3 @@ setup(
     version='0.1',
     packages=find_packages(),
 )
-
-#pip install git+https://github.com/ucef08/lib_ucef08.git
-#pip install --upgrade git+https://github.com/ucef08/lib_ucef08.git
-
-# sd
